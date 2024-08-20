4214afd9c91f152fe9ca1140dae28af34c538a58
Dresnite <giantquartz@gmail.com>
2024-08-20 23:54:40 +0200

    Prepare express server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,13 @@
-const message: string = 'Hello world'
+import app from './app'
 
-console.log(message)
+const DEFAULT_PORT: number = 3001
+
+const port: number = parseInt(process.env.PORT ?? '') || DEFAULT_PORT
+
+if(isNaN(port)) {
+    throw new Error('The value assigned to the env PORT is not a valid number')
+}
+
+app.listen(port, () => {
+    console.log(`Torch's backend is running in the port ${port}`)
+});
